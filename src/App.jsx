9dbad0586a0a1663b66a5f64d048330cc38e54f9
DavidZarfati <zarda2001@gmail.com>
2025-12-02 16:27:37 +0100

    feat:aggiunta lista di array Univoci

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from 'react';
 import './App.css';
 const filmsIniziali = [
@@ -10,33 +11,47 @@ const filmsIniziali = [
 ];
 
 
+
+
 function App() {
+  // Calcola i generi unici iniziali
+  const generiIniziali = filmsIniziali.reduce((acc, film) => {
+    if (!acc.includes(film.genre)) acc.push(film.genre);
+    return acc;
+  }, []);
+
   const [nuovoTitolo, impostaNuovoFilm] = useState("");
   const [nuovoGenere, impostaNuovoGenere] = useState("");
   const [films, impostaFilms] = useState(filmsIniziali);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filmsFiltrati, setFilmsFiltrati] = useState(filmsIniziali);
+  const [generiUnici, setGeneriUnici] = useState(generiIniziali);
 
   function gestisciInvio(event) {
     event.preventDefault();
     if (nuovoTitolo.trim()) {
-      console.log('SUBMIT: ', nuovoTitolo, nuovoGenere);
       impostaFilms([...films, { title: nuovoTitolo, genre: nuovoGenere }]);
+      // Aggiorna generiUnici solo se il nuovo genere non è già presente
+      if (nuovoGenere.trim() && !generiUnici.includes(nuovoGenere)) {
+        setGeneriUnici([...generiUnici, nuovoGenere]);
+      }
       impostaNuovoFilm('');
       impostaNuovoGenere('');
     }
   }
 
   useEffect(() => {
-    const filmsFiltrati = filmsIniziali.filter(film => {
-      const search = searchTerm.toLowerCase();
-      return (
-        film.title.toLowerCase().includes(search) ||
-        film.genre.toLowerCase().includes(search)
-      );
-    });
-    impostaFilms(filmsFiltrati);
-  }, [searchTerm]);
-
+    const filtrati = searchTerm.length === 0
+      ? films
+      : films.filter(film => {
+        const search = searchTerm.toLowerCase();
+        return (
+          film.title.toLowerCase().includes(search) ||
+          film.genre.toLowerCase().includes(search)
+        );
+      });
+    setFilmsFiltrati(filtrati);
+  }, [searchTerm, films]);
 
   return (
     <div className="App">
@@ -54,11 +69,19 @@ function App() {
 
       <div>
         <ul>
-          {films.map((film, index) => (
+          {filmsFiltrati.map((film, index) => (
             <li key={film.title + film.genre + index}>
               <h2>{film.title}</h2>
               <h4>Genere: {film.genre}</h4>
             </li>
+          ))}
+        </ul>
+      </div>
+      <div className='array-generi'>
+        {/* <h3>Lista dei generi disponibili</h3> */}
+        <ul>
+          {generiUnici.map((genere, index) => (
+            <li key={genere + index}>{genere}</li>
           ))}
         </ul>
       </div>
