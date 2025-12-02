5567c921b143edf9cac0f278a68884ba50cc8887
DavidZarfati <zarda2001@gmail.com>
2025-12-02 15:20:15 +0100

    feat:creata lista film, con possibilita di aggiungere film con titolo e  genere

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-const films = [
+const filmsIniziali = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
   { title: 'Batman', genre: 'Azione' },
   { title: 'Interstellar', genre: 'Fantascienza' },
   { title: 'Pulp Fiction', genre: 'Thriller' },
-]
+];
+
 
 function App() {
+  const [nuovoTitolo, impostaNuovoFilm] = useState("");
+  const [nuovoGenere, impostaNuovoGenere] = useState("");
+  const [films, impostaFilms] = useState(filmsIniziali);
+  function gestisciInvio(event) {
+    event.preventDefault();
+    if (nuovoTitolo.trim()) {
+      console.log('SUBMIT: ', nuovoTitolo, nuovoGenere);
+      impostaFilms([...films, { title: nuovoTitolo, genre: nuovoGenere }]);
+      impostaNuovoFilm('');
+      impostaNuovoGenere('');
+    }
+  }
+
+
   return (
     <div className="App">
+      <form onSubmit={gestisciInvio}>
+        <input type="text" placeholder='titolo' value={nuovoTitolo} onChange={(event) => { impostaNuovoFilm(event.target.value) }} />
+        <input type="text" placeholder='genere' value={nuovoGenere} onChange={(event) => { impostaNuovoGenere(event.target.value) }} />
+        <button type="submit">Aggiungi Film</button>
+      </form>
+
+
       <div>
         <ul>
           {films.map((film, index) => (
-
-            <li key={film.id}>
+            <li key={film.title + film.genre + index}>
               <h2>{film.title}</h2>
-              <h4>Genere:{film.genre}</h4>
+              <h4>Genere: {film.genre}</h4>
             </li>
-
           ))}
         </ul>
       </div>
