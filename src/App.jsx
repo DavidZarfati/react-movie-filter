229da884a045ac8e15df303301c3469d8845d823
DavidZarfati <zarda2001@gmail.com>
2025-12-02 15:04:06 +0100

    feat:map che stampa titolo e genere del film

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,75 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-
-function Contatore() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(`Il valore di count è cambiato a: ${count}`);
-  }, [count]);
-  const incrementa = () => {
-    setCount(count + 1);
-  };
-  const decrementa = () => {
-    setCount(count - 1);
-  };
-  return (
-    <div>
-      <h1>Contatore: {count}</h1>
-      <button onClick={incrementa}>Incrementa</button>
-      <button onClick={decrementa}>Decrementa</button>
-    </div>
-  );
-}
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
   return (
     <div className="App">
-      <Contatore />
+      <div>
+        <ul>
+          {films.map((film, index) => (
+
+            <li key={film.id}>
+              <h2>{film.title}</h2>
+              <h4>Genere:{film.genre}</h4>
+            </li>
+
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Contatore() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     console.log(`Il valore di count è cambiato a: ${count}`);
+//   }, [count]);
+//   const incrementa = () => {
+//     setCount(count + 1);
+//   };
+//   const decrementa = () => {
+//     setCount(count - 1);
+//   };
+//   return (
+//     <div>
+//       <h1>Contatore: {count}</h1>
+//       <button onClick={incrementa}>Incrementa</button>
+//       <button onClick={decrementa}>Decrementa</button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Contatore />
+//     </div>
+//   );
+// }
+
+// export default App;
